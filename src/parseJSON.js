7fcd9a96044d1c6d26dc29ef3cf5c899e5fed2d9
bkim121 <bkim121@gmail.com>
2017-08-21 20:12:23 -0700

    Advanced part in progress

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,74 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
+  console.log(json);
+
+  if (json.length <= 1) {
+    return json;
+  }
+  
+  if (json[0] === '[') {
+    var result = [];
+    
+    return result;
+  }
+
+  if (json[0] === '{') {
+    var result = {};
+    var place = [];
+    var json = json.slice(1, json.length);
+    while (json.length > 2){
+      var hold = parseJSON(json);
+      place.push(hold);
+      json = json.slice(hold.length + 2);
+    }
+    
+    for (var i = 0; i < place.length - 1; i++) {
+      console.log();
+      if (place[i + 1] === "") {
+        result[place[i]] = "";
+        i++;
+      } else {
+        result[place[i]] = result[place[i + 1]];
+        i++;
+      } 
+    }
+    return result;
+  }
+  
+  if (json[0] === '"') {
+    var i = 1;
+    var string = '';
+    while (json[i] !== '"') {
+      string += json[i];
+      i++;
+    }
+    return string;
+  }
+
+  if (json[0] === ',') {
+  
+  }
+  
+
+  if (json[0] === ':') {
+    json = json.slice(2);
+    return parseJSON(json);
+  }
+  
+  
   // your code goes here
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
